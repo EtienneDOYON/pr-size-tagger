adbb224a3711a29e9bc1208725f2056f5e883f51
etienne-doyon <etienne.doyon@bedrockstreaming.com>
2022-04-29 14:56:34 +0200

    Final debug & cleanup

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ npm install
 # Run the bot
 npm start
 ```
+
+## Settings
+
+> To customize the bot, you can edit the `settings.json` file in the root of the project.
+> Please however note that after editing the file, you need to restart the bot for the modifications to apply.
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
- * @param {import('./settings.json')} settings
  */
 
-module.exports = (app, settings) => {
+module.exports = (app) => {
 
   //#region JSON SETTINGS
+  let settings = require('./settings.json');
+
   const COMMENTS_ENABLED = settings.comments_enabled;
-/*
+
   const WEIGHT_OF_ADDITION = settings.weights.line_addition_weight;
   const WEIGHT_OF_DELETION = settings.weights.line_deletion_weight;
   const WEIGHT_OF_FILE = settings.weights.file_edited_weight;
@@ -17,57 +18,51 @@ module.exports = (app, settings) => {
   const M_BREAKPOINT = settings.sizeBreakpoints.medium_size;
   const L_BREAKPOINT = settings.sizeBreakpoints.large_size;
   const XL_BREAKPOINT = settings.sizeBreakpoints.extra_large_size;
-  */
-  //#endregion
-
-  const WEIGHT_OF_ADDITION = 0;
-  const WEIGHT_OF_DELETION = 0;
-  const WEIGHT_OF_FILE = 0;
   
-  const S_BREAKPOINT = 0;
-  const M_BREAKPOINT = 0;
-  const L_BREAKPOINT = 0;
-  const XL_BREAKPOINT = 0;
-
+  //#endregion
 
   app.on("pull_request", async (context) => {
 
     const payload = context.payload;
     const pull_request = payload.pull_request;
 
-    console.log("New pull request action detected: " + payload.action);
-
     switch(payload.action) {
-
       case ("assigned"):
       case ("opened"):
-      case ("reopened"):
       case ("ready_for_review"):
+      case ("reopened"):
       case ("edited"):
-      case ("synchronize"):
-        const sizeTag = getTotalPrSize(pull_request.deletions, pull_request.additions, pull_request.changed_files);
+      case ("synchronize"):    
+        // If the PR is still a draft, we do not want to add a tag yet
+        if (pull_request.draft == true) break;
 
+        const [totalSize, sizeTag] = getTotalPrSize(pull_request.deletions, pull_request.additions, pull_request.changed_files);
+        const currentLabel = pull_request.labels.find(label => (label.name == "XS" || label.name == "S" || label.name == "M"|| label.name == "L" || label.name == "XL")
+                                  && label.name != sizeTag);
+
+        // If the PR currently has a size tag, and it is different from the one we want, then we remove it first.
+        if (currentLabel != null) {
+            context.octokit.rest.issues.removeLabel(context.issue({name: currentLabel.name}))
+          }
+
+        // If comments are enabled, we tell the user what are we doing, and why
         if (COMMENTS_ENABLED) {
           var _body = "The total value of your PR is " + totalSize + ".\n"
             + "Therefore, this PR is considered " + sizeTag;
 
           context.octokit.rest.issues.createComment(context.issue({body: _body}))
         }
-        context.octokit.rest.issues.addLabels(context.issue({labels: [sizeTag]}))
-        break;
 
-      default:
-        return;
+        // If the tag we need isn't on the PR yet, then we add it.
+        if (pull_request.labels.find(label => label.name == sizeTag) == null)
+          context.octokit.rest.issues.addLabels(context.issue({labels: [sizeTag]}))
+        break;
     }
 
 
   });
 
   function getTotalPrSize(deletions, additions, changed_files) {
-    console.log("deletions: " + deletions);
-    console.log("additions: " + additions);
-    console.log("changed_files: " + changed_files);
-
     const size = additions * WEIGHT_OF_ADDITION
       + deletions * WEIGHT_OF_DELETION
       + changed_files * WEIGHT_OF_FILE;
@@ -78,8 +73,6 @@ module.exports = (app, settings) => {
     if (size > M_BREAKPOINT) sizeTag = "M";
     if (size > L_BREAKPOINT) sizeTag = "L";
     if (size > XL_BREAKPOINT) sizeTag = "XL";
-    return sizeTag;
+    return [size, sizeTag];
   }
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
