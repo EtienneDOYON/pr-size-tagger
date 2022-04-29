b2ae25aaf22d123e28227677979f60166786470c
etienne-doyon <etienne.doyon@bedrockstreaming.com>
2022-04-29 13:48:54 +0200

    Some cleanup + start working on JSON settings

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pr-size-tagger
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) that Add tags to PRs when opened, indicating its size
+> This Github app aims to label PRs accordingly to their size, so that there is more clarity within the project itself.
 
 ## Setup
 
@@ -11,23 +11,3 @@ npm install
 # Run the bot
 npm start
 ```
-
-## Docker
-
-```sh
-# 1. Build container
-docker build -t pr-size-tagger .
-
-# 2. Start container
-docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> pr-size-tagger
-```
-
-## Contributing
-
-If you have suggestions for how pr-size-tagger could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
-
-For more, check out the [Contributing Guide](CONTRIBUTING.md).
-
-## License
-
-[ISC](LICENSE) © 2022 etienne-doyon <etienne.doyon@bedrockstreaming.com>
--- index.js
+++ index.js
@@ -1,21 +1,85 @@
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
+ * @param {import('./settings.json')} settings
  */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+module.exports = (app, settings) => {
+
+  //#region JSON SETTINGS
+  const COMMENTS_ENABLED = settings.comments_enabled;
+/*
+  const WEIGHT_OF_ADDITION = settings.weights.line_addition_weight;
+  const WEIGHT_OF_DELETION = settings.weights.line_deletion_weight;
+  const WEIGHT_OF_FILE = settings.weights.file_edited_weight;
+  
+  const S_BREAKPOINT = settings.sizeBreakpoints.small_size;
+  const M_BREAKPOINT = settings.sizeBreakpoints.medium_size;
+  const L_BREAKPOINT = settings.sizeBreakpoints.large_size;
+  const XL_BREAKPOINT = settings.sizeBreakpoints.extra_large_size;
+  */
+  //#endregion
+
+  const WEIGHT_OF_ADDITION = 0;
+  const WEIGHT_OF_DELETION = 0;
+  const WEIGHT_OF_FILE = 0;
+  
+  const S_BREAKPOINT = 0;
+  const M_BREAKPOINT = 0;
+  const L_BREAKPOINT = 0;
+  const XL_BREAKPOINT = 0;
+
+
+  app.on("pull_request", async (context) => {
+
+    const payload = context.payload;
+    const pull_request = payload.pull_request;
+
+    console.log("New pull request action detected: " + payload.action);
+
+    switch(payload.action) {
+
+      case ("assigned"):
+      case ("opened"):
+      case ("reopened"):
+      case ("ready_for_review"):
+      case ("edited"):
+      case ("synchronize"):
+        const sizeTag = getTotalPrSize(pull_request.deletions, pull_request.additions, pull_request.changed_files);
+
+        if (COMMENTS_ENABLED) {
+          var _body = "The total value of your PR is " + totalSize + ".\n"
+            + "Therefore, this PR is considered " + sizeTag;
+
+          context.octokit.rest.issues.createComment(context.issue({body: _body}))
+        }
+        context.octokit.rest.issues.addLabels(context.issue({labels: [sizeTag]}))
+        break;
+
+      default:
+        return;
+    }
+
+
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  function getTotalPrSize(deletions, additions, changed_files) {
+    console.log("deletions: " + deletions);
+    console.log("additions: " + additions);
+    console.log("changed_files: " + changed_files);
 
+    const size = additions * WEIGHT_OF_ADDITION
+      + deletions * WEIGHT_OF_DELETION
+      + changed_files * WEIGHT_OF_FILE;
+
+    var sizeTag = "";
+    if (size <= S_BREAKPOINT) sizeTag = "XS";
+    if (size > S_BREAKPOINT) sizeTag = "S";
+    if (size > M_BREAKPOINT) sizeTag = "M";
+    if (size > L_BREAKPOINT) sizeTag = "L";
+    if (size > XL_BREAKPOINT) sizeTag = "XL";
+    return sizeTag;
+  }
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 };
